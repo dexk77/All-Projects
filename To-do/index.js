cc606886768c498eb82c03242d5a10ad485c420c
dexk77 <77209345+dexk77@users.noreply.github.com>
2024-06-20 21:01:34 +0530

    first commit

---
+++ To-do/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+const tasks = [];
+
+// Your code for API endpoints will go here
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+  });
+
+  // 
+  
+  app.post('/tasks', (req, res) => {``
+    const newTask = {
+      id: tasks.length + 1,
+      task: req.body.task,
+      is_completed:req.body.is_completed
+    };
+    tasks.push(newTask);
+    res.json({ message: 'Task added successfully!' });
+  });
+  
+  app.put('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const updatedTask = req.body;
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+  
+    if (taskIndex !== -1) {
+      tasks[taskIndex] = { ...tasks[taskIndex], ...updatedTask };
+      res.json({ message: 'Task updated successfully!' });
+    } else {
+      res.status(404).json({ message: 'Task not found!' });
+    }
+  });
+  
+  app.delete('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+  
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
+      res.json({ message: 'Task deleted successfully!' });
+    } else {
+      res.status(404).json({ message: 'Task not found!' });
+    }
+  });
+  
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
